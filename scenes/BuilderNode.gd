61cf7570d9beae35e5cc3498ad56724259b9a4dc
TobiMercader <Tobimercader@gmail.com>
2024-04-14 12:10:05 -0300

    mesh library created and gridmap set up

--- scenes/BuilderNode.gd
+++ scenes/BuilderNode.gd
@@ -8,10 +8,17 @@ var mouseWorld
 var building = preload("res://scenes/test_building.tscn")
 
 func _process(delta):
+	#gets the mouse position relative to the screen
 	var posMouse = get_viewport().get_mouse_position()
+	#creates a ray that goes in the direction between the center of the camera and the point
+	#of the mouse on hte camera frame
 	var rayMouseFrom = camera.project_ray_origin(posMouse)
+	#cerates a point that goes from the previous point to a point in the direction of the ray 
+	#but 100 points foward 
 	var rayMouseTo =  rayMouseFrom + camera.project_ray_normal(posMouse) * 100
+	#creates a ray query, used to get info from the ray
 	var rayQuery = PhysicsRayQueryParameters3D.create(rayMouseFrom, rayMouseTo)
+	#reference to the world space (coordinates etc..)
 	var worldSpace = world.get_world_3d().direct_space_state
 	#gets the point that intersected with a collider
 	var rayResult = worldSpace.intersect_ray(rayQuery)
